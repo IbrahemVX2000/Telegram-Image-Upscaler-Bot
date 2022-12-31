4abafe1b62cd899fd11ca508daf81544a74afade
IbrahemVX2000 <121667290+IbrahemVX2000@users.noreply.github.com>
2022-12-31 22:37:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from image_enhancer import imageEnhancer
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-TOKEN = "<your token here>"
+TOKEN = "<t.me/upscalingPhoto_bot.>"
 
 
 def start(update, context):
